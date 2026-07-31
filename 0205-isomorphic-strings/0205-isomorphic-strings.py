e5ec9802c662d922b3f5b9df35a6c32905007783
Subha Patra <137531449+Subha9635@users.noreply.github.com>
2026-07-31 16:29:09 +0530

    Time: 7 ms (36.24%), Space: 19.4 MB (9.24%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        m1, m2 = [0]*256, [0]*256
+        for i in range(len(s)):
+            if m1[ord(s[i])] != m2[ord(t[i])]:
+                return False
+            m1[ord(s[i])] = i+1
+            m2[ord(t[i])] = i+1
+        
+        return True
