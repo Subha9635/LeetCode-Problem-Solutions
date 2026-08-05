49012a275d5034eda1d01e3da899f143a8b5c9c1
Subha Patra <137531449+Subha9635@users.noreply.github.com>
2026-08-06 00:57:38 +0530

    Time: 7 ms (96.45%), Space: 19.6 MB (91.36%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -2,38 +2,23 @@ class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         board = [["."] * n for _ in range(n)]
         ans = []
-        self.solver(0,board,ans,n)
+        lefthash, upperdiag, lowerdiag = [0]*n, [0]*(2*n-1), [0]*(2*n-1)
+        self.solver(0, board, ans, n, lefthash, lowerdiag, upperdiag)
         return ans
-        
 
-    def solver(self, col, board, ans, n):
+    def solver(self, col, board, ans, n, lefthash, lowerdiag, upperdiag):
         if col == n:
             ans.append(["".join(row) for row in board])
             return
-        for row in range(n):
-            if self.isSafe(row,col,board,n):
-                board[row][col] = "Q"
-                self.solver(col+1,board,ans,n)
-                board[row][col] = "."
-
-    def isSafe(self, row, col, board, n):
-        duprow, dupcol = row, col
-        while row>=0 and col>= 0:  #Checks Upper Diagonal
-            if board[row][col] == "Q":
-                return False
-            row -= 1
-            col -= 1
         
-        row, col = duprow, dupcol
-        while row<n and col>=0:  #Checks lowerd diagonal
-            if board[row][col] == "Q":
-                return False
-            row += 1
-            col -= 1
-
-        row, col = duprow, dupcol
-        while col>=0:  #Checks Left side
-            if board[row][col] == "Q":
-                return False
-            col -= 1
-        return True
+        for row in range(n):
+            if lefthash[row] == 0 and lowerdiag[row+col] == 0 and upperdiag[(n-1)+(col-row)] == 0:
+                lefthash[row] = 1
+                lowerdiag[row+col] = 1
+                upperdiag[(n-1)+(col-row)] = 1
+                board[row][col] = "Q"
+                self.solver(col+1,board,ans,n,lefthash,lowerdiag,upperdiag)
+                board[row][col] = "."
+                lefthash[row] = 0
+                lowerdiag[row+col] = 0
+                upperdiag[(n-1)+(col-row)] = 0
