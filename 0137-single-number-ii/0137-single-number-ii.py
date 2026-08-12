26c6303eb8a7032581c7631603a26c6e18be545d
Subha Patra <137531449+Subha9635@users.noreply.github.com>
2026-08-13 00:04:18 +0530

    Time: 0 ms (100%), Space: 20.6 MB (61.78%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,14 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ans = 0
-        for bitindex in range(0,32): #iterates for all 32 bits of integer
-            count = 0
-            for i in range(0,len(nums)):   #Iterates the array
-                if nums[i] & (1<<bitindex):
-                    count += 1
-            if count%3 == 1: #Means one element apperaing once
-                ans = ans | (1<<bitindex)
-            
-        if ans >= 2**31:
-            ans -= 2**32
-        return ans
+        nums.sort()
+        i = 1
+        while i<len(nums):
+            if nums[i-1] == nums[i]:
+                i += 3
+            else:
+                break
+        return nums[i-1]
