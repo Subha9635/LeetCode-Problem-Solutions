78016d9070b8e2521c44b3c2358fc95381214663
Subha Patra <137531449+Subha9635@users.noreply.github.com>
2026-08-13 00:27:20 +0530

    Time: 2 ms (77.31%), Space: 20.7 MB (44.96%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,6 +1,11 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ones, twos = 0, 0
+        ones, twos = 0, 0 #Declaring two buckets
+        """
+        num will go to ones if it is not in twos
+        num will go to twos if it is in ones
+        Another bucket thrice will be needed iff the thrice appearing elements are required to be given as output
+        """
         for num in nums:
             ones = (ones^num) & ~twos
             twos = (twos^num) & ~ones
