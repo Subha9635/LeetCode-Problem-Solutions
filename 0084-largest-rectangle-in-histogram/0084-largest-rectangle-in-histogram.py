cd9915367d20bb77c7e0751219002887bac16cc3
Subha Patra <137531449+Subha9635@users.noreply.github.com>
2026-08-24 23:40:29 +0530

    Time: 129 ms (36.67%), Space: 31.3 MB (53.6%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,40 +1,22 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        NSE = self.findNSE(heights)
-        PSE = self.findPSE(heights)
-        maxi = 0
+        stack = []
+        maxarea = 0
+
         for i in range(len(heights)):
-            maxi = max(maxi, heights[i]*(NSE[i]-PSE[i]-1))
-        return maxi
-    
-    def findNSE(self, arr):
-        n = len(arr)
-        NSE = [n]*n
-        stack = []
-        i = n-1
-        while i >= 0:
-            while stack and arr[i] <= arr[stack[-1]]:
+            while stack and heights[stack[-1]] > heights[i]:
+                element = stack[-1]
                 stack.pop()
-            if not stack:
-                NSE[i] = n
-            else:
-                NSE[i] = stack[-1]
+                NSE = i
+                PSE = -1 if not stack else stack[-1]
+                maxarea = max(maxarea, heights[element]*(NSE-PSE-1))
             stack.append(i)
-            i -= 1
-        return NSE
         
-    def findPSE(self, arr):
-        n = len(arr)
-        PSE = [-1]*n
-        stack = []
-        i = 0
-        while i < n:
-            while stack and arr[i] <= arr[stack[-1]]:
-                stack.pop()
-            if not stack:
-                PSE[i] = -1
-            else:
-                PSE[i] = stack[-1]
-            stack.append(i)
-            i += 1
-        return PSE
+        while stack:
+            NSE = len(heights)
+            element = stack[-1]
+            stack.pop()
+            PSE = -1 if not stack else stack[-1]
+            maxarea = max(maxarea, heights[element]*(NSE-PSE-1))
+        
+        return maxarea
