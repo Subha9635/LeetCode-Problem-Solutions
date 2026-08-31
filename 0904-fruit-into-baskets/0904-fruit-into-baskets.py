0d2a8495ec02c536d19da495203466b8225fe2e2
Subha Patra <137531449+Subha9635@users.noreply.github.com>
2026-08-31 21:38:34 +0530

    Time: 202 ms (22.36%), Space: 25.8 MB (84.93%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        maxlen = 0
+        mapp = defaultdict(int)
+        l, r = 0, 0
+        while r<len(fruits):
+            mapp[fruits[r]] += 1
+            while len(mapp) > 2:
+                mapp[fruits[l]] -= 1
+                if mapp[fruits[l]] == 0:
+                    del mapp[fruits[l]]
+                l += 1
+            if len(mapp) <= 2:
+                maxlen = max(maxlen,r-l+1)
+            r += 1
+        return maxlen
