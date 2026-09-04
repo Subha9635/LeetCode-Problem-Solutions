f88fd68bb7c628f596852bd0435ef4016f6b1d46
Subha Patra <137531449+Subha9635@users.noreply.github.com>
2026-09-04 21:03:42 +0530

    Time: 77 ms (80.93%), Space: 19.7 MB (52.51%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -3,15 +3,12 @@ class Solution:
         hasharr = [0]*26
         l, r, maxlen, maxfreq = 0, 0, 0, 0
         while r<len(s):
-            hasharr[ord(s[r])-ord('A')] += 1    #Freq gets updated
-            maxfreq = max(maxfreq, hasharr[ord(s[r])-ord('A')])
-            while (r-l+1)-maxfreq > k:
-                hasharr[ord(s[l])-ord('A')] -= 1
-                maxfreq = 0
-                for i in range(26):
-                    maxfreq = max(maxfreq,hasharr[i])
+            hasharr[ord(s[r])-65] += 1
+            maxfreq = max(maxfreq, hasharr[ord(s[r])-65])
+            if (r-l+1)-maxfreq>k:
+                hasharr[ord(s[l])-65] -= 1
                 l += 1
-            if (r-l+1)-maxfreq <= k:
+            else:
                 maxlen = max(maxlen, r-l+1)
             r += 1
         return maxlen
